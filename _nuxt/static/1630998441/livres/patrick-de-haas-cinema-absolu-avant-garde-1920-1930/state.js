99343045a9b8dc6f7f7ae919c98500937d16f53d
ApolloNet <ApolloNet@users.noreply.github.com>
2021-09-08 09:28:30 +0000

    deploy: 271fc6ee38195209c17dba32746599eb31f6ce09

--- _nuxt/static/1630998441/livres/patrick-de-haas-cinema-absolu-avant-garde-1920-1930/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630998441",layout:"default",error:a,serverRendered:true,routePath:"\u002Flivres\u002Fpatrick-de-haas-cinema-absolu-avant-garde-1920-1930",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e7518478"}}}}(null));
---
+++ _nuxt/static/1631093308/revue/mettray-s02-n06/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1631093308",layout:"default",error:a,serverRendered:true,routePath:"\u002Frevue\u002Fmettray-s02-n06",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7374d178"}}}}(null));
