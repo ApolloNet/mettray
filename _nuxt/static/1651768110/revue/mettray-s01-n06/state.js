081b8f8ce930f39ce327d55f3bcf5735b760454b
ApolloNet <ApolloNet@users.noreply.github.com>
2022-09-24 12:53:13 +0000

    deploy: acfa0e9e7f31d34865d4b39954a94db9dce1e9d3

--- _nuxt/static/1651768110/revue/mettray-s01-n06/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1651768110",layout:"default",error:a,serverRendered:true,routePath:"\u002Frevue\u002Fmettray-s01-n06",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d2f8f4c8"}}}}(null));
